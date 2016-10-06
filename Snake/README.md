243245f8d60d01549b33048f8cc1306df5c723d9
Jamie S <jamieshi2u@gmail.com>
2016-10-05 21:45:41 -0700

    Code for snake game

---
+++ Snake/README.md
@@ -0,0 +1,2 @@
+# Snake
+Snake game written in C using SDL2
---
+++ Snake/menu.h
@@ -0,0 +1,23 @@
+//
+//  menu.h
+//  Snake
+//
+//  Created on 9/17/16.
+//
+
+#ifndef menu_h
+#define menu_h
+
+#include "main.h"
+#include <stdio.h>
+
+void drawScore(SDL_Renderer *renderer, GameState *game);
+
+void setAlpha(SDL_Texture *texture, Uint8 alpha);
+void noWallsActivated(SDL_Renderer *renderer, GameState *game, int canGoThrough);
+
+void pauseGame(SDL_Renderer *renderer, GameState *game);
+
+void playAgainButtons(SDL_Renderer *renderer, GameState *game, int mouseOverYes, int mouseOverNo);
+
+#endif /* menu_h */
